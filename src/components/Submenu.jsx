5629494b2a9006acf12d8165a4db22e54ef31bea
lnmrn <lnmrn.dev@gmail.com>
2026-08-13 14:01:23 +0200

    add sublink component

--- src/components/Submenu.jsx
+++ src/components/Submenu.jsx
@@ -1,6 +1,28 @@
 //only seen on bigger screens!
+import pages from "../data";
+import LinkItem from "./LinkItem";
+import { useGlobalContext } from "../GlobalContext";
+
 function Submenu() {
-  return <h2>Submenu</h2>;
+  const { pageId, setPageId } = useGlobalContext();
+  const currentPage = pages.find((page) => page.pageId === pageId);
+
+  return (
+    <div className="submenu">
+      <h5>{currentPage?.page}</h5>
+      <div
+        style={{
+          gridTemplateColumns:
+            currentPage?.links?.length > 3 ? "1fr 1fr" : "1fr",
+        }}
+        className="submenu-links"
+      >
+        {currentPage?.links?.map((link) => (
+          <LinkItem key={link.id} link={link} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Submenu;
